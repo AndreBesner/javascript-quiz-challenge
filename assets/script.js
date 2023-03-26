92515bb23225bd33404eece21a7ae2f7b9c159c7
Andre Besner <besner.andre@gmail.com>
2023-03-26 13:26:38 -0400

    make countdown work on click, cleanup

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,62 @@
 var overAllTime = 60 ; // this is the main value for the one minute quiz
 
+// Variable for end game view score button
+var endGameViewScore = document.querySelector('.end-game');
 
-var startButton = document.querySelector('#start-button');
+// variable for initial div on webpage
+var quizBoxStart = document.querySelector('.quiz-box-start');
+
+// variable for questions div that is hidden by default, appear when start button clicked, disappear at test end
+var quizBoxQuestions = document.querySelector('.quiz-box-questions');
+
+// variable for final page div,  hidden by dault, appear at test end, of when end button clicked
+var quizBoxEnd = document.querySelector('.quiz-box-end');
+
+// variable for start button to initialize the test
+var startButton = document.querySelector('#start-button'); // this makes a variable for start button element
 
 
-    
+   
+
+/* This is the function to initialize the overall time for test (60 seconds)
+a global variable is declared to run timer off of, so we can cancel that timer with a later function*/
+var myTimer; //global scope to be clearInterval() later
+function timeDown(){
+    myTimer = setInterval(()=>{
+        overAllTime--;
+        document.querySelector('.time-left').innerHTML = overAllTime ; // prints time left to page
+        }   , 1000)
+}
 
 
 
-startButton.addEventListener('click', function(){
-    // this actually hides that start buttun                                                          !!!! REMEMBER TO UNDHIDE LATER !!!
-    startButton.setAttribute('hidden', true);
-    // this is the function to subtract one second from quiz and write it to top of page
-    var timer = setInterval(() => {
-        overAllTime -- ;
-        document.querySelector('.time-left').innerHTML = overAllTime ;
-    }, 1000);
+
+
+
+
+
+
+
+//  this function waits for a click event on start button then begins countdown timer and prints that to page
+//  LOGIC TO BE ADDED
+startButton.addEventListener('click', testStart);
+
+function testStart(){
+        quizBoxStart.setAttribute('hidden', true); // hides start page
+        quizBoxQuestions.style.display = 'block' ; // reveals quiz questions
+        timeDown(); // calls time down function to begin running and quiz has started
+}
+
+// this function runs when user clicks end game view scores button
+// used an arrow function here for practice and it doesn't really needs it's own name 
+endGameViewScore.addEventListener('click', ()=>{
+    testEnd(); //calls following test end function
+    clearInterval(timer); // stops timer with clearInterval()
 });
+
+function testEnd(){
+    quizBoxStart.setAttribute('hidden', true);
+    quizBoxQuestions.style.display = 'none' ;
+    quizBoxEnd.style.display = 'block';
+    clearInterval(myTimer);
+}
