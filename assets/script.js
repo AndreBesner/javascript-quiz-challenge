75ce29d9f41e8c9a72ba58a5aa5979a8d675bf9d
Andre Besner <besner.andre@gmail.com>
2023-03-29 19:58:25 -0400

    add logic to button press, check for time and question overflow, fix overflows

--- assets/script.js
+++ assets/script.js
@@ -29,26 +29,6 @@ var questionArray = [
 ];
    
 
-// /* This is the function to initialize the overall time for test (60 seconds)
-// a global variable is declared to run timer off of, so we can cancel that timer with a later function*/
-// var myTimer; //global scope to be clearInterval() later
-// function timeDown(){
-//     myTimer = setInterval(()=>{
-//         overAllTime--;
-//         document.querySelector('.time-left').innerHTML = overAllTime ; // prints time left to page
-//         }   , 1000)
-// }
-
-// //  this function waits for a click event on start button then begins countdown timer and prints that to page
-// //  LOGIC TO BE ADDED
-// startButton.addEventListener('click', testStart);
-
-// function testStart(){
-//     quizBoxStart.setAttribute('hidden', true); // hides start page
-//     quizBoxQuestions.style.display = 'block' ; // reveals quiz questions
-//     timeDown(); // calls time down function to begin running and quiz has started
-//     printQuestion();
-// }
 
 
 
@@ -65,19 +45,26 @@ function timeDecrease(){
         userTimeRemaining -= 1 ;
         document.querySelector("#time-left").innerHTML = userTimeRemaining ;
         console.log(userTimeRemaining);
+        if(userTimeRemaining <= 0 || currentQuestionIndex > questionArray.length){
+            clearInterval(timer);
+            endGame();
+        }
     }, 1000)
 }
-
 var quizBoxStartPage = document.querySelector("#quiz-box-start-page");
 var quizBoxQuestionsPage = document.querySelector("#quiz-box-questions-page");
 var displayQuestion = document.querySelector("#display-question");
 var displayAnswers = document.querySelector("#display-answers");
 var currentQuestionIndex = 0 ;
 function showQuestions(){
-    quizBoxStartPage.style.display = "none";
-    quizBoxQuestionsPage.style.display = "block";
-    displayQuestion.innerHTML = questionArray[currentQuestionIndex].question;
-    showResponses();
+    if(currentQuestionIndex == 4){
+        endGame();
+        return;
+    }        
+        quizBoxStartPage.style.display = "none";
+        quizBoxQuestionsPage.style.display = "block";
+        displayQuestion.innerHTML = questionArray[currentQuestionIndex].question;
+        showResponses();
 }
 function showResponses(){
     var responses = questionArray[currentQuestionIndex].answers;
@@ -86,17 +73,38 @@ function showResponses(){
         var makeButton = document.createElement("button");
         makeButton.textContent = response;
         makeButton.addEventListener("click", function(e){
-            console.log(e.target.innerText);
-            checkAnswer(e);
+            // console.log(e.target.innerText);
+            checkAnswer(e.target.innerText);
         });
         displayAnswers.appendChild(makeButton);
         
        
+    }
 }
-}
+var userScore = 0;
 function checkAnswer(userSelected){
     var correctAnswerIndex = questionArray[currentQuestionIndex].answer
     var correctAnswer = questionArray[currentQuestionIndex].answers[correctAnswerIndex];
-    console.log(correctAnswer);
-    console.log(correctAnswerIndex);
+    // console.log(correctAnswer);
+    // console.log(correctAnswerIndex);
+    if(userSelected == correctAnswer){
+        console.log("wow it works");
+        userScore++;
+        // console.log(userScore);
+    }
+    else{
+        console.log("wrong");
+        userTimeRemaining -= 5 ;
+        console.log(userTimeRemaining);
+    }
+    displayQuestion.innerHTML = "";
+    displayAnswers.innerHTML = "";
+    currentQuestionIndex++;
+    
+    showQuestions();
+
+}
+
+function endGame(){
+
 }
